9f78719dd051367229deb91735b67dce955cf37e
Winston  Chung <18.chung.winston@ctkprep.org>
2020-04-27 17:53:02 +0000

    Automatically backed up by Learn

--- index.js
+++ index.js
@@ -1,1 +1,8 @@
 // Your code here
+function saturdayFun(act="roller-skate") {
+  return(`This Saturday, I want to ${act}!`)
+}
+
+function mondayWork(act="go to the office") {
+  return(`This Monday, I will ${act}.`)
+}
